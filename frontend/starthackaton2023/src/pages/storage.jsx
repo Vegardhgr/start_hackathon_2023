5bac2c5312814a639430cb9e9ee6a594a44eeb47
jonaf <jifollan@stud.ntnu.no>
2023-10-27 21:37:41 +0200

    made it look more like the prototype

--- frontend/starthackaton2023/src/pages/storage.jsx
+++ frontend/starthackaton2023/src/pages/storage.jsx
@@ -1,16 +1,28 @@
 import "./storage.css"
+import arrow from "../imgs/rightArrow.png"
+import plus from "../imgs/plus.png"
 function Storage() {
     return(
     <div>
         <h1>DINE VARER</h1>
+        <hr />
+        <div class="search-container">
+  <form action="/search" method="get">
+    <input type="text" name="q" placeholder="Search..." />
+    <input type="submit" value="Search" />
+  </form>
+</div>
+
         <div class="buttons">
         <select class="dropdown-button"name="storagetype" id="storagetype">
+        <img src={arrow} height={10}></img>
             <option class="dropdown-select" value="kjøleskap">Kjøleskap</option>
             <option class="dropdown-select"value="fryser">Fryser</option>
             <option class="dropdown-select"value="pantry">Pantry</option>
         </select>
-        <button class="dropdown-button"id="leggtil">Legg til</button>
+        <button class="dropdown-button"id="leggtil"> <img src={plus} height={10}></img> Legg til</button>
         </div>  
+
     </div>
     );
 }
