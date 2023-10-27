4e6aa65457d2fcb2eb5358d0c958ffa134470c70
jonaf <jifollan@stud.ntnu.no>
2023-10-27 20:49:02 +0200

    Added buttons on storage page and css

--- frontend/starthackaton2023/src/pages/storage.jsx
+++ frontend/starthackaton2023/src/pages/storage.jsx
@@ -1,4 +1,17 @@
+import "./storage.css"
 function Storage() {
-
+    return(
+    <div>
+        <h1>DINE VARER</h1>
+        <div class="buttons">
+        <select class="dropdown-button"name="storagetype" id="storagetype">
+            <option class="dropdown-select" value="kjøleskap">Kjøleskap</option>
+            <option class="dropdown-select"value="fryser">Fryser</option>
+            <option class="dropdown-select"value="pantry">Pantry</option>
+        </select>
+        <button class="dropdown-button"id="leggtil">Legg til</button>
+        </div>  
+    </div>
+    );
 }
 export default Storage;
