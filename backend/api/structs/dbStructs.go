42f7abf3122c2fefa21a4ea61327e5582471e822
DanVi007 <danvitrinh@outlook.com>
2023-10-28 18:48:07 +0200

    fixed string error in struct

--- backend/api/structs/dbStructs.go
+++ backend/api/structs/dbStructs.go
@@ -1,21 +1,19 @@
 package structs
 
-
 type Recipe struct {
-	Id int `json:"id"`
-	Name string `json:"name"`
-	MealTime string `json:"mealTime"`
-	Information string `json:"information"`
-	Image string `json:"image"`
-	TimeItTakes string `json:"time_it_takes"`
-	RoastingTime string `json:"roasting_time"`
-	RestTime string `json:"rest_time"`
-	Rating string `json:"rating"`
-	Difficulty string `json:"difficulty"`
+	Id           int    `json:"id"`
+	Name         string `json:"name"`
+	MealTime     string `json:"mealTime"`
+	Information  string `json:"information"`
+	Image        string `json:"image"`
+	TimeItTakes  int    `json:"time_it_takes"`
+	RoastingTime int    `json:"roasting_time"`
+	RestTime     int    `json:"rest_time"`
+	Rating       int    `json:"rating"`
+	Difficulty   string `json:"difficulty"`
 }
 
-
 type Ingredient struct {
-	Name string `json:"name"`
-	Quantity int `json:"quantity"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
 }
