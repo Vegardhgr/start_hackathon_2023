8a3a9d71ce651af81f04b85f6b9c674b1d587468
Vegardhgr <vegard.groder@gmail.com>
2023-10-29 12:49:56 +0100

    Added home page

---
+++ frontend/starthackaton2023/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div width = "100px" style = {{display:"flex", justifyContent: "center", height: "100vh", alignItems: "center",  color:"green"}}>
+            <h1 style= {{dispaly:"inline-block", fontSize: "40px"}}>Food</h1>
+            <h1 style= {{fontSize: "40px"}}>Manager</h1>
+        </div>
+    )
+}  
+
+export default Home;
